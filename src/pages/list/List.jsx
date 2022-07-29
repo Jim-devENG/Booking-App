25eee3c5d3106e8a3d672a8727b00e5f1c6f97ec
James Enietan <enietanjamie@gmail.com>
2022-07-29 20:35:52 +0100

    Add files via upload

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import './list.css'
 import Header from "../../components/header/Header";
 import Navbar from "../../components/navbar/Navbar";
 
@@ -7,6 +7,18 @@ const List = () => {
     <div>
       <Navbar />
       <Header type="list" />
+      <div className='listContainer'>
+        <div className="listWrapper">
+          <div className="listSearch">
+            <h1 className='lsTitle'>Search</h1>
+            <div className='isItem'>
+              <label>Destinatoin</label>
+              <input type="text" />
+            </div>
+          </div>
+          <div className='listResult'></div>
+        </div>
+      </div>
     </div>
   );
 };
